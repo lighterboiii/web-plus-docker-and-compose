c54c09d201cd1220b958039318a008c46b9ce1a6
Vyacheslav Baumtrok <s.baumtrok2009@yandex.ru>
2023-09-07 12:25:53 +0300

    внести правки в конфигурационные файлы

--- backend/src/config/dbConfig.ts
+++ backend/src/config/dbConfig.ts
@@ -12,7 +12,7 @@ export const createDbConfig = (
     type: 'postgres',
     host: configService.get('POSTGRES_HOST'),
     port: configService.get('POSTGRES_PORT'),
-    username: configService.get('POSTGRES_USERNAME'),
+    username: configService.get('POSTGRES_USER'),
     password: configService.get('POSTGRES_PASSWORD'),
     database: configService.get('POSTGRES_DB'),
     entities: [User, Offer, Wish, WishList],
